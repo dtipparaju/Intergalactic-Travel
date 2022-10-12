4ed26b3de3bcab8671b89b2125fada934c6475de
Dhanush Tipparaju <dhanushtips@gmail.com>
2022-10-12 09:52:44 -0500

    Initial Commit

---
+++ Intergalactic Travel/Intergalactic_TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intergalactic_TravelApp.swift
+//  Intergalactic Travel
+//
+//  Created by Dhanush Tipparaju on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Intergalactic_TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
